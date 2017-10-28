e6ec9c363b45c1aaf7717ed06e5d9eba90dc25cb
RoqueOrtega <ortegaroque@gmail.com>
2017-10-28 08:55:09 -0300

    Agregado de carpeta Publish

---
+++ ProcedimientoUNE-EN50128/Publish/scripts/processElementData.js
@@ -0,0 +1,21 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_ERn5EbsdEee0oJqAmnTP4gActivity"]="./../../UNE-EN50128_plug-in/deliveryprocesses/resources/Garantia de Calidad_F8F7A3C7_f8f7a3c7_Activity.jpeg"
+contentPage.processPage.elementUrls["_d7CjQLsdEee0oJqAmnTP4g"] = ["2) Redactar Informe de Verificación de Garantía de Calidad del Software","UNE-EN50128_plug-in/deliveryprocesses/2 Redactar Informe de Verificacion de Garantia de Calidad del So_E35C18CB.html"];
+contentPage.processPage.elementUrls["_djEIILsdEee0oJqAmnTP4g"] = ["Plan de Garantía de Calidad del Software (S)","UNE-EN50128_plug-in/deliveryprocesses/Plan de Garantia de Calidad del Software S_8D930363.html"];
+contentPage.processPage.elementUrls["_djDhErsdEee0oJqAmnTP4g"] = ["Documentos del ciclo de vida","UNE-EN50128_plug-in/deliveryprocesses/Documentos del ciclo de vida E_74826F0D.html"];
+contentPage.processPage.elementUrls["_djDhELsdEee0oJqAmnTP4g"] = ["Verificador (VER)","UNE-EN50128_plug-in/deliveryprocesses/Verificador VER_7B4B7AE7.html"];
+contentPage.processPage.elementUrls["_djBr4LsdEee0oJqAmnTP4g"] = ["1) Redactar Plan de Garantía de Calidad del Software","UNE-EN50128_plug-in/deliveryprocesses/1 Redactar Plan de Garantia de Calidad del Software_A6FF9BA4.html"];
+contentPage.processPage.elementUrls["_ERn5EbsdEee0oJqAmnTP4g"] = ["Garantia de Calidad","UNE-EN50128_plug-in/deliveryprocesses/Garantia de Calidad_F8F7A3C7.html"];
+contentPage.processPage.elementUrls["_d7CjQbsdEee0oJqAmnTP4g"] = ["Informe de Verificación de Garantía de Calidad del Software (S)","UNE-EN50128_plug-in/deliveryprocesses/Informe de Verificacion de Garantia de Calidad del Software S_D1F52CE1.html"];
