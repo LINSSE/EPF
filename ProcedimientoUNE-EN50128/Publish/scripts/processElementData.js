9e421875247b322e3e0ec35ecd0535c1d0189c85
RoqueOrtega <ortegaroque@gmail.com>
2017-10-30 18:04:48 -0300

    Modificaciones publicación

--- ProcedimientoUNE-EN50128/Publish/scripts/processElementData.js
+++ ProcedimientoUNE-EN50128/Publish/scripts/processElementData.js
@@ -12,10 +12,20 @@
 // This file will include dynamically generated process layout related data from 
 // the publishing service 
 contentPage.processPage.imageFiles["_ERn5EbsdEee0oJqAmnTP4gActivity"]="./../../UNE-EN50128_plug-in/deliveryprocesses/resources/Garantia de Calidad_F8F7A3C7_f8f7a3c7_Activity.jpeg"
-contentPage.processPage.elementUrls["_d7CjQLsdEee0oJqAmnTP4g"] = ["2) Redactar Informe de Verificación de Garantía de Calidad del Software","UNE-EN50128_plug-in/deliveryprocesses/2 Redactar Informe de Verificacion de Garantia de Calidad del So_E35C18CB.html"];
-contentPage.processPage.elementUrls["_djEIILsdEee0oJqAmnTP4g"] = ["Plan de Garantía de Calidad del Software (S)","UNE-EN50128_plug-in/deliveryprocesses/Plan de Garantia de Calidad del Software S_8D930363.html"];
-contentPage.processPage.elementUrls["_djDhErsdEee0oJqAmnTP4g"] = ["Documentos del ciclo de vida","UNE-EN50128_plug-in/deliveryprocesses/Documentos del ciclo de vida E_74826F0D.html"];
-contentPage.processPage.elementUrls["_djDhELsdEee0oJqAmnTP4g"] = ["Verificador (VER)","UNE-EN50128_plug-in/deliveryprocesses/Verificador VER_7B4B7AE7.html"];
+contentPage.processPage.imageFiles["_ERn5EbsdEee0oJqAmnTP4gActivityDetail"]="./../../UNE-EN50128_plug-in/deliveryprocesses/resources/Garantia de Calidad_F8F7A3C7_f8f7a3c7_ActivityDetail.jpeg"
+contentPage.processPage.elementUrls["_laVcAb2yEeeUatOPAmFQuA"] = ["Jefe de proyecto (PM)","UNE-EN50128_plug-in/deliveryprocesses/Jefe de proyecto PM_F71D8090.html"];
+contentPage.processPage.elementUrls["_laU08b2yEeeUatOPAmFQuA"] = ["Implementador (IMP)","UNE-EN50128_plug-in/deliveryprocesses/Implementador IMP_2D693DA7.html"];
 contentPage.processPage.elementUrls["_djBr4LsdEee0oJqAmnTP4g"] = ["1) Redactar Plan de Garantía de Calidad del Software","UNE-EN50128_plug-in/deliveryprocesses/1 Redactar Plan de Garantia de Calidad del Software_A6FF9BA4.html"];
-contentPage.processPage.elementUrls["_ERn5EbsdEee0oJqAmnTP4g"] = ["Garantia de Calidad","UNE-EN50128_plug-in/deliveryprocesses/Garantia de Calidad_F8F7A3C7.html"];
+contentPage.processPage.elementUrls["_laTm1L2yEeeUatOPAmFQuA"] = ["Evaluador (ASR)","UNE-EN50128_plug-in/deliveryprocesses/Evaluador ASR_F8CA6F56.html"];
+contentPage.processPage.elementUrls["_d7CjQLsdEee0oJqAmnTP4g"] = ["2) Redactar Informe de Verificación de Garantía de Calidad del Software","UNE-EN50128_plug-in/deliveryprocesses/2 Redactar Informe de Verificacion de Garantia de Calidad del So_E35C18CB.html"];
+contentPage.processPage.elementUrls["_laS_wb2yEeeUatOPAmFQuA"] = ["Diseñador (DES)","UNE-EN50128_plug-in/deliveryprocesses/Disenador DES_805428F9.html"];
 contentPage.processPage.elementUrls["_d7CjQbsdEee0oJqAmnTP4g"] = ["Informe de Verificación de Garantía de Calidad del Software (S)","UNE-EN50128_plug-in/deliveryprocesses/Informe de Verificacion de Garantia de Calidad del Software S_D1F52CE1.html"];
+contentPage.processPage.elementUrls["_ERn5EbsdEee0oJqAmnTP4g"] = ["Garantia de Calidad","UNE-EN50128_plug-in/deliveryprocesses/Garantia de Calidad_F8F7A3C7.html"];
+contentPage.processPage.elementUrls["_laTm0b2yEeeUatOPAmFQuA"] = ["Encargado de los ensayos (TST)","UNE-EN50128_plug-in/deliveryprocesses/Encargado de los ensayos TST_22D1A14D.html"];
+contentPage.processPage.elementUrls["_djDhELsdEee0oJqAmnTP4g"] = ["Verificador (VER)","UNE-EN50128_plug-in/deliveryprocesses/Verificador VER_7B4B7AE7.html"];
+contentPage.processPage.elementUrls["_djEIILsdEee0oJqAmnTP4g"] = ["Plan de Garantía de Calidad del Software (S)","UNE-EN50128_plug-in/deliveryprocesses/Plan de Garantia de Calidad del Software S_8D930363.html"];
+contentPage.processPage.elementUrls["_laUN4b2yEeeUatOPAmFQuA"] = ["Gestor de Configuración","UNE-EN50128_plug-in/deliveryprocesses/Gestor de Configuracion_351929C9.html"];
+contentPage.processPage.elementUrls["_laU09L2yEeeUatOPAmFQuA"] = ["Integrador (INT)","UNE-EN50128_plug-in/deliveryprocesses/Integrador INT_3620BB0.html"];
+contentPage.processPage.elementUrls["_laVcBL2yEeeUatOPAmFQuA"] = ["Validador (VAL)","UNE-EN50128_plug-in/deliveryprocesses/Validador VAL_CD164E99.html"];
+contentPage.processPage.elementUrls["_laUN5L2yEeeUatOPAmFQuA"] = ["Gestor de requisitos (RQM)","UNE-EN50128_plug-in/deliveryprocesses/Gestor de requisitos RQM_B11F7D2.html"];
+contentPage.processPage.elementUrls["_djDhErsdEee0oJqAmnTP4g"] = ["Documentos del ciclo de vida","UNE-EN50128_plug-in/deliveryprocesses/Documentos del ciclo de vida E_74826F0D.html"];
