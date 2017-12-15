a1e55c4e2a0aa3d300ad0d4c9c555d856e99d258
Emanuel <emanuelirrazabal@gmail.com>
2017-12-15 12:37:29 -0300

    Agregar banner
    Banner

---
+++ Publish/scripts/processElementData.js
@@ -0,0 +1,87 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_SHQ1obqNEeexnaZ05SXmkw,_h_P0EL9FEeeXR7irnfn8JwActivity"]="./../../UNE-EN50128_plug-in/deliveryprocesses/resources/FaseEnsayos_BF0B32DA_4ffb602d_Activity.jpeg"
+contentPage.processPage.imageFiles["_SHQ1obqNEeexnaZ05SXmkwActivity"]="./../../UNE-EN50128_plug-in/deliveryprocesses/resources/Ciclo de Vida en V_2ADFEBFF_2adfebff_Activity.jpeg"
+contentPage.processPage.imageFiles["_SHQ1obqNEeexnaZ05SXmkw,_pLktYL6AEeeiGtSEVKM1dAActivity"]="./../../UNE-EN50128_plug-in/deliveryprocesses/resources/New Phase_2_5C9DAF1B_ed8ddc6e_Activity.jpeg"
+contentPage.processPage.imageFiles["_SHQ1obqNEeexnaZ05SXmkw,_yEo_0L6AEeeiGtSEVKM1dA,_xxf9Ac95EeezQ7b2X4kDfwActivity"]="./../../UNE-EN50128_plug-in/deliveryprocesses/resources/Iteracion Planificacion_53B8EE41_a26525c3_Activity.jpeg"
+contentPage.processPage.imageFiles["_SHQ1obqNEeexnaZ05SXmkw,_yEo_0L6AEeeiGtSEVKM1dAActivity"]="./../../UNE-EN50128_plug-in/deliveryprocesses/resources/FasePlanificacion_352A5DB_9442d32e_Activity.jpeg"
+contentPage.processPage.imageFiles["_SHQ1obqNEeexnaZ05SXmkw,_SpZEYL9FEeeXR7irnfn8JwActivity"]="./../../UNE-EN50128_plug-in/deliveryprocesses/resources/FaseImplementacion_D2ED4057_63dd6daa_Activity.jpeg"
+contentPage.processPage.imageFiles["_SHQ1obqNEeexnaZ05SXmkw,_WNcn4LqNEeexnaZ05SXmkwActivity"]="./../../UNE-EN50128_plug-in/deliveryprocesses/resources/FaseDesarrollo_9F4EC3F1_303ef144_Activity.jpeg"
+contentPage.processPage.imageFiles["_SHQ1obqNEeexnaZ05SXmkw,_pcWswbqNEeexnaZ05SXmkwActivity"]="./../../UNE-EN50128_plug-in/deliveryprocesses/resources/FaseRequistos_546F3F91_e55f6ce4_Activity.jpeg"
+contentPage.processPage.elementUrls["_DmYtUL9HEeeXR7irnfn8Jw"] = [" FASE 9 - Validación del Software","UNE-EN50128_plug-in/deliveryprocesses/FaseValidacion_4761D899.html"];
+contentPage.processPage.elementUrls["_JpsDQcPLEeeLc_rF2D1YWQ"] = ["Informe de Ensayos de Integración del Software/Hardware","UNE-EN50128_plug-in/deliveryprocesses/Informe de Ensayos de Integracion del Software Hardware_F325490F.html"];
+contentPage.processPage.elementUrls["_gZCsEcP3EeeLc_rF2D1YWQ"] = ["Informe de Ensayos del Software en Conjunto","UNE-EN50128_plug-in/deliveryprocesses/Informe de Ensayos del Software en Conjunto_71D21ACE.html"];
+contentPage.processPage.elementUrls["_SpZEYL9FEeeXR7irnfn8Jw"] = ["FASE 6- Implementación de Componentes ","UNE-EN50128_plug-in/deliveryprocesses/FaseImplementacion_D2ED4057.html"];
+contentPage.processPage.elementUrls["_aqGPgcM3EeeLc_rF2D1YWQ"] = ["F 1.4-Plan de Validación del Software","UNE-EN50128_plug-in/deliveryprocesses/Plan de Validacion del Software_63543445.html"];
+contentPage.processPage.elementUrls["_UH1sYcAWEeepstfmonv3dA"] = ["Descripción de la Arquitectura del Sistema","UNE-EN50128_plug-in/deliveryprocesses/Descripcion de la Arquitectura del Sistema_9065C780.html"];
+contentPage.processPage.elementUrls["_F1w6UcP6EeeLc_rF2D1YWQ"] = ["Informe de Evaluación del Software","UNE-EN50128_plug-in/deliveryprocesses/Informe de Evaluacion del Software_780C5BA8.html"];
+contentPage.processPage.elementUrls["_bpj9sMi3Eeek0ZpXlk_Y3Q"] = ["Jefe de proyecto (PM)","UNE-EN50128_plug-in/deliveryprocesses/Jefe de proyecto PM_C79F992F.html"];
+contentPage.processPage.elementUrls["_OSsA8cPGEeeLc_rF2D1YWQ"] = ["Informe de Verificación del Diseño de los Componentes Software","UNE-EN50128_plug-in/deliveryprocesses/Informe de Verificacion del Diseno de los Componentes Software_C9E6D0FE.html"];
+contentPage.processPage.elementUrls["_skSsUcPIEeeLc_rF2D1YWQ"] = ["Código Fuente del Software y Documentación de Apoyo","UNE-EN50128_plug-in/deliveryprocesses/Codigo Fuente del Software y Documentacion de Apoyo_1255D149.html"];
+contentPage.processPage.elementUrls["_LEzyIcM2EeeLc_rF2D1YWQ"] = ["Especificación de la Arquitectura del Software","UNE-EN50128_plug-in/deliveryprocesses/Especificacion de la Arquitectura del Software_E2A52601.html"];
+contentPage.processPage.elementUrls["_hw4LcL9GEeeXR7irnfn8Jw"] = [" FASE 8 - Integración del Software","UNE-EN50128_plug-in/deliveryprocesses/FaseIntegracion_79455F8D.html"];
+contentPage.processPage.elementUrls["_bpj9oMi3Eeek0ZpXlk_Y3Q"] = ["Verificador (VER)","UNE-EN50128_plug-in/deliveryprocesses/Verificador VER_B55810B3.html"];
+contentPage.processPage.elementUrls["_dQW8U8i3Eeek0ZpXlk_Y3Q"] = ["Informe de Verificación de Garantía de Calidad del Software (S)","UNE-EN50128_plug-in/deliveryprocesses/Informe de Verificacion de Garantia de Calidad del Software S_A8705349.html"];
+contentPage.processPage.elementUrls["_DJzEIcPKEeeLc_rF2D1YWQ"] = ["Informe de Ensayos de los Componentes Software","UNE-EN50128_plug-in/deliveryprocesses/Informe de Ensayos de los Componentes Software_91FB3B50.html"];
+contentPage.processPage.elementUrls["_xxf9Ac95EeezQ7b2X4kDfw"] = ["Iteración Planificación [1...n]","UNE-EN50128_plug-in/deliveryprocesses/Iteracion Planificacion_53B8EE41.html"];
+contentPage.processPage.elementUrls["_bpj9qsi3Eeek0ZpXlk_Y3Q"] = ["Gestor de requisitos (RQM)","UNE-EN50128_plug-in/deliveryprocesses/Gestor de requisitos RQM_37B2C917.html"];
+contentPage.processPage.elementUrls["_muXvIcM2EeeLc_rF2D1YWQ"] = ["Especificacion de Ensayos de Integracion del Software/Hardware","UNE-EN50128_plug-in/deliveryprocesses/Especificacion de Ensayos de Integracion del Software Hardware_6A129ECF.html"];
+contentPage.processPage.elementUrls["_h_P0EL9FEeeXR7irnfn8Jw"] = [" FASE 7 - Ensayo de Componentes","UNE-EN50128_plug-in/deliveryprocesses/FaseEnsayos_BF0B32DA.html"];
+contentPage.processPage.elementUrls["_ZQfoQcAWEeepstfmonv3dA"] = ["Plan de Seguridad del Sistema","UNE-EN50128_plug-in/deliveryprocesses/Plan de Seguridad del Sistema_3BABD7D3.html"];
+contentPage.processPage.elementUrls["_dQWVQci3Eeek0ZpXlk_Y3Q"] = ["Verificador (VER)","UNE-EN50128_plug-in/deliveryprocesses/Verificador VER_3CA88996.html"];
+contentPage.processPage.elementUrls["_4QosAcM2EeeLc_rF2D1YWQ"] = ["Informe de Verficación de Diseño y Arquitectura de Software","UNE-EN50128_plug-in/deliveryprocesses/Informe de Verficacion de Diseno y Arquitectura de Software_F2C59792.html"];
+contentPage.processPage.elementUrls["_bpj9pMi3Eeek0ZpXlk_Y3Q"] = ["Encargado de los ensayos (TST)","UNE-EN50128_plug-in/deliveryprocesses/Encargado de los ensayos TST_79E9F2D2.html"];
+contentPage.processPage.elementUrls["_ftxucb9KEeeXR7irnfn8Jw"] = ["Especificación Requisitos del Sistema","UNE-EN50128_plug-in/deliveryprocesses/Especificacion Requisitos_13BC600B.html"];
+contentPage.processPage.elementUrls["_CMl0QcM2EeeLc_rF2D1YWQ"] = ["Informe de Verificación de los Requisitos del Software","UNE-EN50128_plug-in/deliveryprocesses/Informe de Verificacion de los Requisitos del Software_60D8BA2F.html"];
+contentPage.processPage.elementUrls["_bpj9osi3Eeek0ZpXlk_Y3Q"] = ["Diseñador (DES)","UNE-EN50128_plug-in/deliveryprocesses/Disenador DES_AE8F04D9.html"];
+contentPage.processPage.elementUrls["_WGmPIcM3EeeLc_rF2D1YWQ"] = ["F 1.3-Plan de Verificación del Software","UNE-EN50128_plug-in/deliveryprocesses/Plan de Verificacion del Software_69EFF55D.html"];
+contentPage.processPage.elementUrls["_dQWVQMi3Eeek0ZpXlk_Y3Q"] = ["Redactar Informe de Verificación de Garantía de Calidad del Software","UNE-EN50128_plug-in/deliveryprocesses/Redactar Informe de Verificacion de Garantia de Calidad del Soft_4E0F7580.html"];
+contentPage.processPage.elementUrls["_bpj9tsi3Eeek0ZpXlk_Y3Q"] = ["Plan de Garantía de Calidad del Software (S)","UNE-EN50128_plug-in/deliveryprocesses/Plan de Garantia de Calidad del Software S_85686F74.html"];
+contentPage.processPage.elementUrls["_WNcn4LqNEeexnaZ05SXmkw"] = ["FASE 2 - Desarrollo del Sistema","UNE-EN50128_plug-in/deliveryprocesses/FaseDesarrollo_9F4EC3F1.html"];
+contentPage.processPage.elementUrls["__0ZWsNCPEeeY4P3Pq2l9Mg"] = ["Hito Planificación","UNE-EN50128_plug-in/deliveryprocesses/Hito Planificacion_DC7ED50A.html"];
+contentPage.processPage.elementUrls["_yEo_0L6AEeeiGtSEVKM1dA"] = ["FASE 1 - Planificacion del Software","UNE-EN50128_plug-in/deliveryprocesses/FasePlanificacion_352A5DB.html"];
+contentPage.processPage.elementUrls["_cDTuIcAWEeepstfmonv3dA"] = ["F 1.1-Plan de Garantia de Calidad del Software","UNE-EN50128_plug-in/deliveryprocesses/Plan de Garantia de Calidad del Software_B6A916BD.html"];
+contentPage.processPage.elementUrls["_Ixd3QMPKEeeLc_rF2D1YWQ"] = ["Informe de Verificación del Código Fuente del Software","UNE-EN50128_plug-in/deliveryprocesses/Informe de Verificacion del Codigo Fuente del Software_4E26AEBF.html"];
+contentPage.processPage.elementUrls["_SHQ1obqNEeexnaZ05SXmkw"] = ["Ciclo de Vida en V","UNE-EN50128_plug-in/deliveryprocesses/Ciclo de Vida en V_2ADFEBFF.html"];
+contentPage.processPage.elementUrls["_9U-iocPFEeeLc_rF2D1YWQ"] = ["Especificación de Diseño de los Componentes Software","UNE-EN50128_plug-in/deliveryprocesses/Especificacion de Diseno de los Componentes_17584EC.html"];
+contentPage.processPage.elementUrls["_RNTPwcP5EeeLc_rF2D1YWQ"] = ["Plan de Evaluación del Software","UNE-EN50128_plug-in/deliveryprocesses/Plan de Evaluacion del Software_2D572733.html"];
+contentPage.processPage.elementUrls["_ybprEcM1EeeLc_rF2D1YWQ"] = ["Especificación de Ensayos del Software en Conjunto","UNE-EN50128_plug-in/deliveryprocesses/Especificacion de Ensayos del Software en Conjunto_DD5CC4D7.html"];
+contentPage.processPage.elementUrls["_3EuTQcPKEeeLc_rF2D1YWQ"] = ["Informe de Ensayos de Integración del Software","UNE-EN50128_plug-in/deliveryprocesses/Informe de Ensayos de Integracion del Software_F7EA6A8.html"];
+contentPage.processPage.elementUrls["_dQW8Uci3Eeek0ZpXlk_Y3Q"] = ["Plan de Garantía de Calidad del Software (S)","UNE-EN50128_plug-in/deliveryprocesses/Plan de Garantia de Calidad del Software S_34F89D74.html"];
+contentPage.processPage.elementUrls["_VnZLYcPLEeeLc_rF2D1YWQ"] = ["Informe de Verificación de la Integración del Software","UNE-EN50128_plug-in/deliveryprocesses/Informe de Verificacion de la Integracion del Software_1B4C4D3A.html"];
+contentPage.processPage.elementUrls["__HYzQcP4EeeLc_rF2D1YWQ"] = ["Registros de Mantenimiento del Software","UNE-EN50128_plug-in/deliveryprocesses/Registros de Mantenimiento del Software_73261EEA.html"];
+contentPage.processPage.elementUrls["_yVCuQOGpEeesytrDgMPbcw"] = ["Construir el entorno de gestión colaborativa del proyecto","UNE-EN50128_plug-in/deliveryprocesses/Construir_el_entorno_de_gestin_colaborativa_del_proyecto_7CD86D7C.html"];
+contentPage.processPage.elementUrls["_GnAUocM3EeeLc_rF2D1YWQ"] = ["F 1.2-Plan de Gestión de la Configuración del Software","UNE-EN50128_plug-in/deliveryprocesses/Plan de Gestion de la Configuracion del Software_4F0C531F.html"];
+contentPage.processPage.elementUrls["_pLktYL6AEeeiGtSEVKM1dA"] = ["FASE 4 - Arquitectura y Diseño ","UNE-EN50128_plug-in/deliveryprocesses/New Phase_2_5C9DAF1B.html"];
+contentPage.processPage.elementUrls["_a2gsQL9EEeeXR7irnfn8Jw"] = ["FASE 5 - Diseño de Componentes ","UNE-EN50128_plug-in/deliveryprocesses/FaseDiseno_5A9CF813.html"];
+contentPage.processPage.elementUrls["_prTsMcP4EeeLc_rF2D1YWQ"] = ["Informe de Validación del Software ","UNE-EN50128_plug-in/deliveryprocesses/Informe de Validacion del Software_3B066605.html"];
+contentPage.processPage.elementUrls["_fo-aQN9EEeeI0exrfmj3cg"] = ["FASE 0 - Construir entorno de gestión","UNE-EN50128_plug-in/deliveryprocesses/FASE 0 - Configurar entorno de gestión_2E6A79EC.html"];
+contentPage.processPage.elementUrls["_pcWswbqNEeexnaZ05SXmkw"] = ["FASE 3 - Requisitos del Software","UNE-EN50128_plug-in/deliveryprocesses/FaseRequistos_546F3F91.html"];
+contentPage.processPage.elementUrls["_bRWt0L9HEeeXR7irnfn8Jw"] = ["FASE 10 - Mantenimiento del Software","UNE-EN50128_plug-in/deliveryprocesses/FaseMantenimiento_7E927767.html"];
+contentPage.processPage.elementUrls["_bpjWkMi3Eeek0ZpXlk_Y3Q"] = ["Redactar Plan de Garantía de Calidad del Software","UNE-EN50128_plug-in/deliveryprocesses/Redactar Plan de Garantia de Calidad del Software_BD07FCD5.html"];
+contentPage.processPage.elementUrls["_Qew5YcM2EeeLc_rF2D1YWQ"] = ["Especificación del Diseño del Software","UNE-EN50128_plug-in/deliveryprocesses/Especificacion del Diseno del Software_75722CB.html"];
+contentPage.processPage.elementUrls["_bpj9qMi3Eeek0ZpXlk_Y3Q"] = ["Gestor de Configuración","UNE-EN50128_plug-in/deliveryprocesses/Gestor de Configuracion_3E7BD4F1.html"];
+contentPage.processPage.elementUrls["_fx0oUL9HEeeXR7irnfn8Jw"] = ["FASE 11 - Evaluación del Software","UNE-EN50128_plug-in/deliveryprocesses/FaseEvaluacion_9277F046.html"];
+contentPage.processPage.elementUrls["_uU5PIcAWEeepstfmonv3dA"] = ["Especificación de Requisitos del Software","UNE-EN50128_plug-in/deliveryprocesses/EspeRequisitos_3BFF6D16.html"];
+contentPage.processPage.elementUrls["_bpj9tMi3Eeek0ZpXlk_Y3Q"] = ["Documentos del ciclo de vida","UNE-EN50128_plug-in/deliveryprocesses/Documentos del ciclo de vida E_8C317B4E.html"];
+contentPage.processPage.elementUrls["_gb-kAcM2EeeLc_rF2D1YWQ"] = ["Especificación de Ensayos de Integracion del Software","UNE-EN50128_plug-in/deliveryprocesses/Especificacion de Ensayos de Integracion del Software_7085C04A.html"];
+contentPage.processPage.elementUrls["_bpj9rsi3Eeek0ZpXlk_Y3Q"] = ["Integrador (INT)","UNE-EN50128_plug-in/deliveryprocesses/Integrador INT_FC44AB36.html"];
+contentPage.processPage.elementUrls["_Y3zn8cM2EeeLc_rF2D1YWQ"] = ["Especificación de la Interfaz del Software","UNE-EN50128_plug-in/deliveryprocesses/Especificacion de la Interfaz del Software_F0AC25A8.html"];
+contentPage.processPage.elementUrls["_GHQQkcPGEeeLc_rF2D1YWQ"] = ["Especificación de Ensayos de los Componentes Software","UNE-EN50128_plug-in/deliveryprocesses/Especificacion de Ensayos de los Componentes_4B5E1F1A.html"];
+contentPage.processPage.elementUrls["_CszawcP5EeeLc_rF2D1YWQ"] = ["Registros de Modificaciones del Software","UNE-EN50128_plug-in/deliveryprocesses/Registros de Modificaciones del Software_27F30372.html"];
+contentPage.processPage.elementUrls["_bpj9psi3Eeek0ZpXlk_Y3Q"] = ["Evaluador (ASR)","UNE-EN50128_plug-in/deliveryprocesses/Evaluador ASR_7320E6F8.html"];
+contentPage.processPage.elementUrls["_bpj9rMi3Eeek0ZpXlk_Y3Q"] = ["Implementador (IMP)","UNE-EN50128_plug-in/deliveryprocesses/Implementador IMP_30DB710.html"];
+contentPage.processPage.elementUrls["_N5ZNIcAWEeepstfmonv3dA"] = ["Especificación Requisitos de Seguridad del Sistema","UNE-EN50128_plug-in/deliveryprocesses/EspecifiSeguridad_99086F6.html"];
+contentPage.processPage.elementUrls["_e5foIcM3EeeLc_rF2D1YWQ"] = ["F 1.5-Plan de Mantenimiento del Software","UNE-EN50128_plug-in/deliveryprocesses/Plan de Mantenimiento del Software_74B3E0C3.html"];
+contentPage.processPage.elementUrls["_bpj9ssi3Eeek0ZpXlk_Y3Q"] = ["Validador (VAL)","UNE-EN50128_plug-in/deliveryprocesses/Validador VAL_C0D68D55.html"];
